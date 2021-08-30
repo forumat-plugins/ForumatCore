1888017ca66c6ae9dff68b49cbb5b8e0dc8ea0ce
Forumat <forumat.kontakt@gmail.com>
2021-08-30 11:15:08 +0200

    added JsonBoard

---
+++ src/main/java/eu/forumat/core/model/json/JsonBoard.java
@@ -0,0 +1,63 @@
+package eu.forumat.core.model.json;
+
+import eu.forumat.core.scoreboard.FastBoard;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Getter
+@RequiredArgsConstructor
+public class JsonBoard {
+
+    private final String[] names;
+    private final int animationTick;
+    private final String[] lines;
+
+    private BukkitTask updater;
+    private AtomicInteger currentAnimationIndex ;
+
+    public FastBoard toBoard(Player player, JavaPlugin plugin, String... placeholders) {
+        Map<String, String> placeholderMap = new HashMap<>();
+        for (int i = 0; i < placeholders.length; i++) {
+            placeholderMap.put(placeholders[i], placeholders[i += 1]);
+        }
+
+        final FastBoard fastBoard = new FastBoard(player);
+        currentAnimationIndex = new AtomicInteger(0);
+        fastBoard.updateTitle(names[currentAnimationIndex.get()]);
+
+        updater = Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> {
+
+            int index = currentAnimationIndex.incrementAndGet();
+            if (index >= names.length) {
+                index = 0;
+                currentAnimationIndex.set(0);
+            }
+            fastBoard.updateTitle(names[index]);
+
+        }, 0, animationTick);
+
+        int i = 0;
+        final List<String> lines = Arrays.stream(this.lines).collect(Collectors.toList());
+        Collections.reverse(lines);
+        for (String line : lines) {
+            String finalLineText = line;
+            for (String placeholder : placeholderMap.keySet()) {
+                finalLineText = finalLineText.replace(placeholder, placeholderMap.get(placeholder));
+            }
+            fastBoard.updateLine(i, ChatColor.translateAlternateColorCodes('&', finalLineText));
+        }
+
+        return fastBoard;
+    }
+
+
+}
